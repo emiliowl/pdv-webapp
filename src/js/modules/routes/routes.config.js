0c1979e8548408a4b1fad9ebf43e0e1996750189
Emilio Murta <emilio.murta@msitbrasil.com>
2015-05-29 10:22:14 -0300

    put cards on work!

--- src/js/modules/routes/routes.config.js
+++ src/js/modules/routes/routes.config.js
@@ -12,25 +12,36 @@
     routesConfig.$inject = ['$stateProvider', '$urlRouterProvider', 'RouteProvider'];
     function routesConfig($stateProvider, $urlRouterProvider, Route) {
 
-      // Default route
-      $urlRouterProvider.otherwise('/app/dashboard');
+        // Default route
+        $urlRouterProvider.otherwise('/app/dashboard');
 
-      // Application Routes States
-      $stateProvider.state('app', {
-          url: '/app',
-          abstract: true,
-          templateUrl: Route.base('app.html'),
-          resolve: {
-            _assets: Route.require('icons', 'toaster', 'animate')
-          }
-      });
-      $stateProvider.state('app.dashboard', {
-          url: '/dashboard',
-          templateUrl: Route.base('dashboard.html'),
-          resolve: {
-            assets: Route.require('ngTable', 'ngTableExport')
-          }
-      });
+        // Application Routes States
+        $stateProvider.state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: Route.base('app.html'),
+            resolve: {
+                _assets: Route.require('icons', 'toaster', 'animate')
+            }
+        });
+
+        $stateProvider.state('app.dashboard', {
+            url: '/dashboard',
+            templateUrl: Route.base('dashboard.html'),
+            resolve: {
+                assets: Route.require('ngTable', 'ngTableExport')
+            }
+        });
+
+        $stateProvider.state('app.sales', {
+            url: '/sales',
+            templateUrl: Route.base('sales.html'),
+            resolve: {
+                assets: Route.require('flot-chart', 'flot-chart-plugins', 'ui.knob', 'loadGoogleMapsJS', function() {
+                    return loadGoogleMaps();
+                })
+            }
+        });
     }
 
 })();
--- src/js/modules/routes/vendor.constants.js
+++ src/js/modules/routes/vendor.constants.js
@@ -10,18 +10,38 @@
         .constant('VENDOR_ASSETS', {
             // jQuery based and standalone scripts
             scripts: {
-              'animate':            ['vendor/animate.css/animate.min.css'],
-              'icons':              ['vendor/font-awesome/css/font-awesome.min.css',
+                'animate':            ['vendor/animate.css/animate.min.css'],
+                'icons':              ['vendor/font-awesome/css/font-awesome.min.css',
                                      'vendor/weather-icons/css/weather-icons.min.css',
-                                     'vendor/feather/webfont/feather-webfont/feather.css']
+                                     'vendor/feather/webfont/feather-webfont/feather.css'],
+                'vectormap':          ['vendor/ika.jvectormap/jquery-jvectormap-1.2.2.min.js',
+                    'vendor/ika.jvectormap/jquery-jvectormap-1.2.2.css'],
+                'vectormap-maps':      ['vendor/ika.jvectormap/jquery-jvectormap-world-mill-en.js',
+                    'vendor/ika.jvectormap/jquery-jvectormap-us-mill-en.js'],
+                'loadGoogleMapsJS':   ['app/plugins/gmap/load-google-maps.js'],
+                'flot-chart':         ['vendor/flot/jquery.flot.js'],
+                'flot-chart-plugins': ['vendor/flot.tooltip/js/jquery.flot.tooltip.min.js',
+                                       'vendor/flot/jquery.flot.resize.js',
+                                       'vendor/flot/jquery.flot.pie.js',
+                                       'vendor/flot/jquery.flot.time.js',
+                                       'vendor/flot/jquery.flot.categories.js',
+                                       'vendor/flot-spline/js/jquery.flot.spline.min.js'],
+                'jquery-ui':          ['vendor/jquery-ui/jquery-ui.min.js',
+                    'vendor/jqueryui-touch-punch/jquery.ui.touch-punch.min.js'],
+                'moment' :            ['vendor/moment/min/moment-with-locales.min.js']
             },
             // Angular modules scripts (name is module name to be injected)
             modules: [
-              {name: 'toaster',           files: ['vendor/angularjs-toaster/toaster.js',
-                                                  'vendor/angularjs-toaster/toaster.css']},
-              {name: 'ngTable',           files: ['vendor/ng-table/dist/ng-table.min.js',
-                                                  'vendor/ng-table/dist/ng-table.min.css']},
-              {name: 'ngTableExport',     files: ['vendor/ng-table-export/ng-table-export.js']},
+                {name: 'toaster',           files: ['vendor/angularjs-toaster/toaster.js',
+                                                    'vendor/angularjs-toaster/toaster.css']},
+
+                {name: 'ui.knob',           files: ['vendor/angular-knob/src/angular-knob.js',
+                                                    'vendor/jquery-knob/dist/jquery.knob.min.js']},
+
+                {name: 'ngTable',           files: ['vendor/ng-table/dist/ng-table.min.js',
+                                                    'vendor/ng-table/dist/ng-table.min.css']},
+
+                {name: 'ngTableExport',     files: ['vendor/ng-table-export/ng-table-export.js']},
             ]
 
         });
